fc38b61b0b3f23fd636ebf798b4a6c2ba141bd86
raphaelcastrofo <raphaelcastrofo@gmail.com>
2025-03-11 22:16:25 +0100

    o que foi feito?
    - Navegacao entre as paginas funcionando com MVVM
    
    -seta na segunda pagina funcionando
    
    -detail service pronto
    
    -conecta com o pokemon clicado, e conecata com a api em "pokemon/name"
    
    -ja esta conectando e pegando os o parametros name, height and weight

---
+++ app/src/main/java/com/example/pokedexhacksprint/detail/data/DetailService.kt
@@ -0,0 +1,12 @@
+package com.example.pokedexhacksprint.detail.data
+
+import com.example.pokedexapp.PokemonDto
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface DetailService {
+    @GET("pokemon/{name}")
+    suspend fun getPokemonByName(
+        @Path("name") name: String): Response<PokemonDto>
+}
---
+++ app/src/main/java/com/example/pokedexhacksprint/detail/presentation/PokeDetailViewModel.kt
@@ -0,0 +1,58 @@
+package com.example.pokedexhacksprint.detail.presentation
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.example.pokedexapp.PokemonDto
+import com.example.pokedexhacksprint.common.data.RetrofitClient
+import com.example.pokedexhacksprint.detail.data.DetailService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class PokeDetailViewModel(
+    private val detailService: DetailService
+): ViewModel() {
+
+    private val _uiPoke = MutableStateFlow<PokemonDto?>(null)
+    val uiPoke: StateFlow<PokemonDto?> = _uiPoke
+
+    fun fetchMovieDetail(pokeName: String) {
+        if (_uiPoke.value == null) {
+            viewModelScope.launch(Dispatchers.IO) {
+                val response = detailService.getPokemonByName(pokeName)
+                if (response.isSuccessful){
+                    _uiPoke.value = response.body()
+                } else {
+                    Log.d(
+                        "MovieDetailViewModel",
+                        "Request Error :: ${response.errorBody()}"
+                    )
+                }
+            }
+        }
+    }
+
+    fun cleanPokemonId() {
+        viewModelScope.launch {
+            delay(1000)
+            _uiPoke.value = null
+        }
+    }
+    companion object {
+        val Factory : ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+                val detailService =
+                    RetrofitClient.retrofitInstance.create(DetailService::class.java)
+                return PokeDetailViewModel(
+                    detailService
+                ) as T
+            }
+        }
+    }
+}
