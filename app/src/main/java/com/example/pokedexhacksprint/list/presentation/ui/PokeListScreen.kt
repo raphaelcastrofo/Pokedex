79054d15e23a35282d92d67755841816c15c24e4
raphaelcastrofo <raphaelcastrofo@gmail.com>
2025-03-14 20:43:31 +0100

    ui clean
    -erro na quebra de palavras

--- app/src/main/java/com/example/pokedexhacksprint/list/presentation/ui/PokeListScreen.kt
+++ app/src/main/java/com/example/pokedexhacksprint/list/presentation/ui/PokeListScreen.kt
@@ -203,7 +203,7 @@ fun PokemonItem(
             )
             Text(
                 text = pokemonDto.name.replaceFirstChar { it.uppercase() }, // Deixa a primeira letra do nome maiscula
-                fontSize = 18.sp,
+                fontSize = 19.sp,
                 fontWeight = FontWeight.SemiBold,
                 color = Color.DarkGray
             )
