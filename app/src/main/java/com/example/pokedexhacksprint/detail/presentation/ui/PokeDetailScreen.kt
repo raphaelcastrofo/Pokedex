e1960cb04ad1b7a56af6c0da3d677650a0cd2921
Thayara Vianello <thayaravianello@criattussolution.com>
2025-03-14 18:11:58 +0100

    Designer melhorado

--- app/src/main/java/com/example/pokedexhacksprint/detail/presentation/ui/PokeDetailScreen.kt
+++ app/src/main/java/com/example/pokedexhacksprint/detail/presentation/ui/PokeDetailScreen.kt
@@ -3,6 +3,7 @@ package com.example.pokedexhacksprint.detail.presentation.ui
 import android.graphics.drawable.Icon
 import android.text.Layout
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,6 +17,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Card
@@ -83,7 +85,7 @@ fun TopBar(pokemon: PokemonDto,
             modifier = Modifier
                 .fillMaxWidth()
                 .background(Color(0xFFECECEC))
-                .padding(16.dp),
+                ,
             horizontalArrangement = Arrangement.Start
         ) {
             IconButton(
@@ -91,7 +93,7 @@ fun TopBar(pokemon: PokemonDto,
                     detailViewModel.cleanPokemonId()
                     navHostController.popBackStack()
                 },
-                modifier = Modifier.padding(top = 16.dp)
+                modifier = Modifier.padding(top = 0.dp)
             ) {
                 Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = " Back Button ")
             }
@@ -114,9 +116,12 @@ fun PokedexScreen(pokemon: PokemonDto) {
 
         Box(
             modifier = Modifier
-                .height(250.dp)
+                .padding(15.dp)
+                .height(220.dp)
                 .fillMaxWidth()
+                .border(10.dp, Color.Transparent, shape = RoundedCornerShape(30.dp))
                 .background(cardBackgroundColor),
+
             contentAlignment = Alignment.Center
         ) {
             AsyncImage(
@@ -184,7 +189,7 @@ fun PokedexScreen(pokemon: PokemonDto) {
 fun TypeBadge(type: String, color: Color) {
     Box(
         modifier = Modifier
-            .padding(4.dp)
+            .padding(10.dp)
             .background(color, shape = RoundedCornerShape(16.dp))
             .padding(horizontal = 16.dp, vertical = 8.dp)
     ) {
@@ -198,12 +203,12 @@ fun TypeBadge(type: String, color: Color) {
 }
 
 val statColorMap = mapOf(
-    "hp" to Color(0xFFFC4C4C), // Vermelho para HP
-    "attack" to Color(0xFF4C91FC), // Azul para Ataque
-    "defense" to Color(0xFF8BC34A), // Verde para Defesa
-    "special-attack" to Color(0xFFFFC107), // Amarelo para Ataque Especial
-    "special-defense" to Color(0xFF9C27B0), // Roxo para Defesa Especial
-    "speed" to Color(0xFF00BCD4) // Ciano para Velocidade
+    "hp" to Color(0xFFBC464D),
+    "attack" to Color(0xFFE6AB4A),
+    "defense" to Color(0xFF7BB2D6),
+    "special-attack" to Color(0xFF9AB1C4),
+    "special-defense" to Color(0xFF8EBA80),
+    "speed" to Color(0xFFF50F58)
 )
 
 @Composable
@@ -211,8 +216,8 @@ fun StatBar(label: String, value: Int, statName: String) {
     val color = statColorMap[statName] ?: Color.Gray
     val maxValue = 255f
     Column(modifier = Modifier
-        .fillMaxWidth()
-        .padding(vertical = 4.dp, horizontal = 16.dp)
+        .fillMaxWidth().padding(horizontal = 25.dp)
+        .padding(vertical = 4.dp, horizontal = 7.dp)
     ){
         Text(text = label, color = Color.DarkGray)
         Row(
@@ -225,14 +230,15 @@ fun StatBar(label: String, value: Int, statName: String) {
                 trackColor = Color.LightGray,
                 modifier = Modifier
                     .weight(1f)
-                    .height(12.dp)
+                    .height(9.dp)
                     .clip(RoundedCornerShape(8.dp))
             )
             Spacer(modifier = Modifier.width(8.dp))
+
             Text(
                 text = "$value / $maxValue",
                 color = Color.DarkGray,
-                fontSize = 16.sp,
+                fontSize = 18.sp,
                 fontWeight = FontWeight.Bold
             )
         }
@@ -244,7 +250,7 @@ fun PokemonInfo(weight: Int, height: Int) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 16.dp),
+            .padding(top = 10.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Spacer(modifier = Modifier.height(8.dp))
